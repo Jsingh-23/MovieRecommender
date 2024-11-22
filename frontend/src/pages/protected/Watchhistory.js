0bbd1e59e2f7b40b08a3239950f59c853d8e7b1e
Jaskaranpal Singh <35478639+Jsingh-23@users.noreply.github.com>
2024-11-21 22:47:28 -0800

    refactored watchhistory and watchlist to use caching, preventing unnecessary api calls
    reduced number of api calls by over 90%

--- frontend/src/pages/protected/Watchhistory.js
+++ frontend/src/pages/protected/Watchhistory.js
@@ -1,60 +1,41 @@
 import { useAuth } from "../../contexts/AuthContext";
-import { useState, useEffect } from "react";
-import { getWatchHistory, removeFromWatchHistory } from "../../services/api";
+import { useState } from "react";
+import { removeFromWatchHistory } from "../../services/api";
 import "../../styles/list.css";
 import MovieCard from "../../components/ui/MovieCard";
-
+import { useWatchHistory } from "../../hooks";
 import { useQueryClient } from "@tanstack/react-query";
 
 const WatchHistoryPage = () => {
     const {user} = useAuth();
-    const [watchHistoryList, setWatchHistoryList] = useState([]);
-    const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
-    const [deleting, setDeleting] = useState(null);
+    const {data: watchhistory} = useWatchHistory(user?.uid);
 
     const queryClient = useQueryClient();
-
-    const getWatchHistoryData = async() => {
-        if (!user) return;
-        setLoading(true);
-        setError(null);
-
-        try {
-            const data = await getWatchHistory(user.uid);
-            setWatchHistoryList(data);
-        } catch (error) {
-            setError("Failed to load watch history");
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-    }
-
+    
     const handleRemoveFromWatchHistory = async(movieId) => {
         if (!user) return;
-        setLoading(true);
-        setError(null);
 
         try {
             await removeFromWatchHistory(user.uid, movieId);
+            queryClient.setQueryData(['watchhistory', user.uid], (oldData) => 
+                oldData.filter(movie => movie.id !== movieId));
         } catch (error) {
             setError("Failed to remove movie from watch history: ", error);
             console.error(error);
-        } finally {
-            setLoading(false);
-            setWatchHistoryList(prev => prev.filter(movie => movie.id !==  movieId));
-        }
+        } 
     }
 
-
-    useEffect(() => {
-        getWatchHistoryData();
-    }, [user]);
+// Useeffect no longer needed because react query handles data fetching on component mount
+// and refetches data when user.uid changes. 
+// Also handles loading and error states and manages cache updates
+    // useEffect(() => {
+    //     getWatchHistoryData();
+    // }, [user]);
 
     return (
         <div className="movies-grid">
-            {watchHistoryList.map((movie, index) => (
+            {watchhistory?.map((movie, index) => (
                 <MovieCard
                     key={index}
                     movie={movie}
--- frontend/src/pages/protected/Watchlist.js
+++ frontend/src/pages/protected/Watchlist.js
@@ -1,6 +1,6 @@
 import { useAuth } from "../../contexts/AuthContext";
-import { useState, useEffect } from "react";
-import { getWatchlist, removeFromWatchlist, addToWatchHistory } from "../../services/api";
+import { useState } from "react";
+import { removeFromWatchlist, addToWatchHistory } from "../../services/api";
 import "../../styles/list.css";
 import MovieCard from "../../components/ui/MovieCard";
 import { useWatchlist } from "../../hooks";
@@ -8,62 +8,41 @@ import { useQueryClient } from "@tanstack/react-query";
 
 const WatchlistPage = () => {
     const {user} = useAuth();
-    // const [watchlist, setWatchlist] = useState([]);
-    const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
-    const [deleting, setDeleting] = useState(null);
+    const [loading, setLoading] = useState(false);
     const {data: watchlist} = useWatchlist(user?.uid)
+
+    console.log("watchlist: ", watchlist);
 
     const queryClient = useQueryClient();
 
-    console.log("watchlist: ", watchlist);
-    
-    // const getWatchlistData = async() => {
-    //     if (!user) return;
-    //     setLoading(true);
-    //     setError(null);
-
-    //     try {
-    //         const data = await getWatchlist(user.uid);
-    //         setWatchlist(data);
-    //     } catch(error) {
-    //         setError("Failed to load watchlist");
-    //         console.error(error);
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // }
-
     const handleRemoveFromWatchlist = async(movieId) => {
         if (!user) return;
-        setLoading(true);
-        setError(null);
 
         try {
             await removeFromWatchlist(user.uid, movieId);
+            queryClient.setQueryData(['watchlist', user.uid], (oldData) => // update cache with the movie removed
+                oldData.filter(movie => movie.id !== movieId));
         } catch (error) {
             setError("Failed to remove movie from watchlist: ", error);
             console.error(error);
-        } finally {
-            setLoading(false);
-            queryClient.setQueryData(['watchlist', user.uid], (oldData) => // update cache with the movie removed
-                oldData.filter(movie => movie.id !== movieId));
-        }
+        } 
     }
 
-    // const handleAddToWatchHistory = async(movieId) => {
-    //     if (!user) return;
-    //     setError(null);
-    //     setLoading(true);
-    //     try {
-    //         await addToWatchHistory(user.uid, movieId);
-    //     } catch (error) {
-    //         setError("Failed to add movie to watch history: ", error);
-    //         console.error(error);
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // }
+    const handleAddToWatchHistory = async(movieId) => {
+        if (!user) return;
+        setError(null);
+        setLoading(true);
+        try {
+            await addToWatchHistory(user.uid, movieId);
+            // queryClient.setQueryData
+        } catch (error) {
+            setError("Failed to add movie to watch history: ", error);
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     // useEffect(() => {
     //     getWatchlistData();
@@ -83,7 +62,7 @@ const WatchlistPage = () => {
                 },
                 {
                   type: 'addToHistory',
-                //   onClick: () => handleAddToWatchHistory(movie)
+                  onClick: () => handleAddToWatchHistory(movie)
                 }
               ]}
             />
